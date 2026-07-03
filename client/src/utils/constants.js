364fd0b62ffd3d66587d0e75b64df0385b1dd8ac
Utkarsh Singh <utkarshsingh@Utkarshs-MacBook-Air.local>
2026-07-03 11:06:58 +0530

    Fix API URL to Railway

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,7 @@
-export const NODE_ENV = "DEV"
-// export const NODE_ENV = "PROD"
-export const SERVER_URL = 'http://localhost:5001'
-//export const SERVER_URL = "https://my-portfolio-hdfd.onrender.com"
+// export const NODE_ENV = "DEV"
+export const NODE_ENV = "PROD"
+// export const SERVER_URL = 'http://localhost:5001'
+export const SERVER_URL = "http://my-portfolio-production-1910.up.railway.app"
 export const CLIENT_URL = "#"
 export const SOCIAL = {
 	LINKEDIN: "https://www.linkedin.com/in/utkarsh-singh25/",
